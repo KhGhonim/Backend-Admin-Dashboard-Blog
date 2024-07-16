94dd60ac819b4411e9d25c026552aa77630e15a4
Khaled <khaledonem89@gmail.com>
2024-07-16 21:32:41 +0300

    v5

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ import postRoutes from "./Api/Routes/postRoutes.js";
 const app = express();
 const port = 3000;
 
-
 // Middleware Connections
 app.use(
   cors({
     origin: "https://admin-dashboard-blog.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    
   })
 );
 
