75c841dbcce989c6c1f41b830cdc3b806dbb009e
Khaled <khaledonem89@gmail.com>
2024-07-17 00:54:26 +0300

    v14

--- Api/Helpers/JWTverifier.js
+++ Api/Helpers/JWTverifier.js
@@ -2,15 +2,16 @@ import jwt from "jsonwebtoken";
 const SECRET_KEY = process.env.JWT_SECRET;
 
 export default function JWTverifier(req, res, next) {
-  const authorization = req.cookies.token;
-  console.log(authorization);
-  if (!authorization) {
+  const authorization = req.headers["authorization"];
+  const token = authorization && authorization.split(" ")[1];
+  console.log(token);
+  if (!token) {
     return res.status(401).json({
       message: "No Authorization Header",
     });
   }
 
-  jwt.verify(authorization, SECRET_KEY, (err, user) => {
+  jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
       return res.status(500).json({
         message: "Invalid Token",
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(
   cors({
     origin:
       "https://admin-dashboard-blog.vercel.app",
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
