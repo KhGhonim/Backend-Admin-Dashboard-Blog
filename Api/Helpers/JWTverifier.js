3d119a96cf4e68916e4b02c203363634d80c80e0
Khaled <khaledonem89@gmail.com>
2024-07-16 23:12:49 +0300

    v7

--- Api/Helpers/JWTverifier.js
+++ Api/Helpers/JWTverifier.js
@@ -2,14 +2,16 @@ import jwt from "jsonwebtoken";
 const SECRET_KEY = process.env.JWT_SECRET;
 
 export default function JWTverifier(req, res, next) {
-  const authorization = req.cookies.token;
-  if (!authorization) {
+  const authorization = req.headers['authorization'];
+  const token = authorization && authorization.split(' ')[1];
+
+  if (!token) {
     return res.status(401).json({
       message: "No Authorization Header",
     });
   }
 
-  jwt.verify(authorization, SECRET_KEY, (err, user) => {
+  jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
       return res.status(500).json({
         message: "Invalid Token",
